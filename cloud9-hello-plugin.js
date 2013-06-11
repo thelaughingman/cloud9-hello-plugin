af7c69404cfde0fc281d4901f9563ed9ca396ec1
Younes <younes@interactive-object.com>
2013-06-11 12:24:36 +0200

    Update cloud9-hello-plugin.js

--- cloud9-hello-plugin.js
+++ cloud9-hello-plugin.js
@@ -1,18 +1,37 @@
 define(function(require, exports, module) {
 
 var ext = require("core/ext");
-var helloDep = require("./dependency.js");
+var menus = require("ext/menus/menus");
+var sys = require('sys')
+var exec = require('child_process').exec;
 
-module.exports = ext.register("ext/helloplugin/helloplugin", {
-    name    : "cloud9-hello-plugin",
-    dev     : "Ajax.org",
+module.exports = ext.register("ext/plugin/killplugin", {
+    name    : "cloud9-kill-plugin",
+    dev     : "Younes",
     type    : ext.GENERAL,
     deps    : [],
     nodes   : [],
     alone   : true,
 
+    killClicked: function() {
+        function puts(error, stdout, stderr) { sys.puts(stdout) }
+        exec("lkill -9 $(lsof -t -i tcp:3001)", puts);
+    },
+        
     hook : function() {
-        console.log(helloDep.getHello());
+        window.console.log("Kill Preview c9ext loaded.");
+            var _self = this;
+            this.nodes.push(
+            menus.addItemByPath("Tools/Kill", new apf.item({
+                onclick: function() {
+                    _self.killClicked();
+                },
+                isAvailable: function(editor) {
+                    //if(window.tabEditors.getPage().name.toString().substr(-3) == ".md")
+                    return true;
+                }
+            }), 0));
+            
     },
 
     enable: function () {},
